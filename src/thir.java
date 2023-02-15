4902550affedf3a560106cb06eb07cb9d1c9b9aa
Kes <kostilio911@gmail.com>
2023-02-15 13:08:27 +0400

    sec commit

--- src/thir.java
+++ src/thir.java
@@ -1,18 +1,28 @@
-public class thir {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+
+public class Main {
     public static void main(String[] args) {
-        String[] words = {"stop", "stop", "stop", "stop", "stop"};
-        boolean flag = true;
-        int max = 0;
-        for (int i = 0; i < words.length - 1; i++) {
-            if (words[i].substring(0) !=words[i + 1].substring(0))flag = false;
+        Logger logger = Logger.getAnonymousLogger();
+        logger.info(String.valueOf(5));
+        try (FileWriter writer = new FileWriter("test.txt")) {
+            String info = "information\n";
+            for (int i = 0; i < 100; i++) {
+                writer.write(String.valueOf(info));
+                logger.info(info);
+            }
+            FileHandler fileHandler =new FileHandler();
+
+
+        } catch (IOException e) {
+
         }
-        if (flag) {
-            for (int i = 0; i < words.length; i++)
-                if (words[i].length() > max) {
-                    max = words[i].length();
-                    System.out.println(max);
-                } else System.out.println(1);
-        }
+
+
     }
 }
 
